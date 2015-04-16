4bc9a3f707c93141dd21665f3cb80252395f1b1d
kouignamann <gu.bertran@gmail.com>
2015-04-16 20:36:10 +0200

    -> Simple particle system done

--- BattleStar/src/fr/kouignamann/battlestar/model/drawable/DrawableParticleSystem.java
+++ BattleStar/src/fr/kouignamann/battlestar/model/drawable/DrawableParticleSystem.java
@@ -17,7 +17,7 @@ import org.lwjgl.util.vector.Vector4f;
 public class DrawableParticleSystem {
 	
 	public static FloatBuffer EMPTY_MAX_PARTICLES_BUFFER = BufferUtils.createFloatBuffer(4*MAX_PRTICULES_PER_SYSTEM);
-	private static float DEFAULT_FERTILITY = 0.5f;
+	private static float DEFAULT_FERTILITY = 10f;
 
 	private int vaoId;
 	private int vboId;
@@ -45,32 +45,27 @@ public class DrawableParticleSystem {
 			DrawableParticuleUnit particle = particlesIt.next();
 			if (particle.getLife() > 0) {
 				particle.setLife(particle.getLife() - deltaMillis*1.0f);
-				particle.setSpeed(particle.getSpeed().translate(0.0f, -4.905f*deltaMillis, 0.0f));
+				particle.setSpeed(particle.getSpeed().translate(0.0f, -0.004905f*deltaMillis, 0.0f));
 				particle.setPosition(particle.getPosition().translate(
-						particle.getSpeed().getX()*deltaMillis,
-						particle.getSpeed().getY()*deltaMillis,
-						particle.getSpeed().getZ()*deltaMillis,
+						particle.getSpeed().getX()*deltaMillis*particle.getWeight(),
+						particle.getSpeed().getY()*deltaMillis*particle.getWeight(),
+						particle.getSpeed().getZ()*deltaMillis*particle.getWeight(),
 						0));
 				particle.setAngle(0);
-//				p.speed += glm::vec3(0.0f,-9.81f, 0.0f) * (float)delta * 0.5f;
-//	            p.pos += p.speed * (float)delta;
 			} else {
 				particlesIt.remove();
 			}
 		}
 
-		System.out.println("New Part count : " + newParticleCount);
-
 		Random random = new Random();
 		while (newParticleCount > 0) {
 			particules.add(new DrawableParticuleUnit(
 					new Vector4f(0, 0, 0, 0), // Initial position
-					new Vector3f(0, 2, 0), // Initial speed
-					new Vector4f(random.nextInt(256)*1.0f/255, random.nextInt(256)*1.0f/255, random.nextInt(256)*1.0f/255, 1),
-					0, 0, 0, 5000));
+					new Vector3f((1-random.nextInt(2)*2)*random.nextInt(1001)*1.0f/1000.0f, 2, (1-random.nextInt(2)*2)*random.nextInt(1001)*1.0f/1000.0f), // Initial speed
+					new Vector4f(random.nextInt(256)*1.0f/255, random.nextInt(256)*1.0f/255, random.nextInt(256)*1.0f/255, 1), // Color
+					0, 0, 0.1f, 1000));			  // size, angle, weight, life
 			newParticleCount--;
 		}
-		System.out.println("Drawn Part count : " + particules.size());
 		
 		GL30.glBindVertexArray(vaoId);
 		GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, positionBufferId);
