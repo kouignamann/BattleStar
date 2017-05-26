e2638d7a4e92b577ebfeafe08398c54f28e070b7
Guillaume BERTRAND <gbertrand@prima-solutions.com>
2017-05-26 17:21:28 +0200

    Chargement auto des libs natives

--- BattleStar/src/fr/kouignamann/battlestar/game/BattleStar.java
+++ BattleStar/src/fr/kouignamann/battlestar/game/BattleStar.java
@@ -5,11 +5,14 @@ import org.lwjgl.opengl.Display;
 import fr.kouignamann.battlestar.core.controlers.Controlers;
 import fr.kouignamann.battlestar.core.graphics.GraphicContext;
 
+import java.lang.reflect.Field;
+
 public class BattleStar {
 
 	private BattleStar() {
 		super();
-		
+
+		initLwjglLibs();
 		GraphicContext.init();
 		Controlers.init();
 		
@@ -26,5 +29,24 @@ public class BattleStar {
 	
 	public static void main(String[] args) {
 		new BattleStar();
+	}
+
+
+
+	public void initLwjglLibs() {
+		try {
+			java.awt.Toolkit.getDefaultToolkit();
+			String javaLibPath = System.getProperty("java.library.path");
+			String pathSeparator = System.getProperty("path.separator");
+			System.setProperty(
+				"java.library.path",
+				javaLibPath + pathSeparator + "lib/natives/");
+			Field sysPath = ClassLoader.class.getDeclaredField("sys_paths");
+			sysPath.setAccessible(true);
+			sysPath.set(null, null);
+		}
+		catch (NoSuchFieldException | IllegalAccessException | UnsatisfiedLinkError e) {
+			System.exit(-1);
+		}
 	}
 }
